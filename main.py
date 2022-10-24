a463032400b17990936336d2f9cbec1f43ad14ad
Marcus Dunn <marcus.s.dunn@gmail.com>
2022-10-24 13:10:19 -0700

    added logging into github

--- main.py
+++ main.py
@@ -1,5 +1,40 @@
+import os
+from logging import getLogger
+from logging import basicConfig
+
+from dotenv import load_dotenv
+from github import Github
+from github import GithubException
+
+GITHUB_ACCESS_TOKEN_KEY: str = "GITHUB_ACCESS_TOKEN"
+logger = getLogger("main")
+
+
 def main() -> None:
-    print("hello world")
+    load_dotenv()
+    configure_logging()
+    github = load_github()
+
+
+def configure_logging() -> None:
+    basicConfig(level=os.environ.get("LOG_LEVEL", "INFO"))
+
+
+def load_github() -> Github | None:
+    access_token = os.environ.get(GITHUB_ACCESS_TOKEN_KEY)
+    if access_token is None:
+        logger.warning("GITHUB_ACCESS_TOKEN_KEY is not set")
+
+    github = Github(access_token)
+
+    try:
+        user = github.get_user().login
+    except GithubException as err:
+        logger.error(f"Getting user failed: {err}")
+        return None
+
+    logger.info(f"Logged in as {user}")
+    return github
 
 
 if __name__ == '__main__':
